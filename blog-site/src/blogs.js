bc9d23d9615d2149768ff2cda272069e49436adf
vimalathithan17 <vimalathithan17@gmail.com>
2023-02-04 04:02:01 +0000

    my learning experience  added

--- blog-site/src/blogs.js
+++ blog-site/src/blogs.js
@@ -5,4 +5,11 @@ export const my_blogs = {
       "date":"2021-03-01",
       "content":"I started my programming journey when I was in secondary school. Back then, I joined an Infocomm club and I started to learn Scratch, a versatile drag-and-drop tool that teaches me simple logic flow, data conditioning and simple iteration loops. It got me so interested in programming that I spent hours making a flight simulator on Scratch.\n\nI was so into programming that I took the computing class for my GCE O Levels. There, I started learning my first real programming language and that was Python. I was learning data structuring, the basics of networking, functions and classes and much more. My computing teacher gave my classes a series of programming questions in a gamified manner. Each time we got an algorithm correct, we were awarded points. My friends and I will spend time after school trying to outdo each other.\n\nWith the experience I had, and the opportunities I was given, I decided to pursue a career in programming. I am currently studying Computer Science at the National University of Singapore. I am currently in my 3rd year and I am looking forward to my final year project and internship opportunities.",
     },
-  }
+    "blog_2":{
+      "title":"My Learning Experience in StackUp",
+      "author":"le vimal",
+      "date":"2021-03-01",
+      "content":"My journey with stackup  as a stackie is something special to me.I would say that stackup had palyed a role directly or indirectly to help me gain the knowledge that i currently posses about programming,web dev,web3,sql and many more.I loved stack up right from the begining and i would say that i joined stackup at the right time and the 30 days of web 2 and 30 days of web 3 werre just about to start.It is because of those learing paths,I ,a peron with zero knoweldge on web dev,have evolved into a decent web developer.Something that really love in particular about stack up is that stackup comes up with new campaigns on a regular intervals which are a sequel to the previous campaigns.And the stackup's discord comunity is one of the best comunities out there.Each one of them are eager to share their knowledge and are ready to help other fellow stackis.And a huge shout out to brother kodyvim ,i love the python challenges that he posts every staurday,it has really helped me to learn more python and the feedback from other stackies on my code and the solution of othes have really helped me and has given me new insights. I love this journey and am sure that it will continue to be awesome!",
+     
+  },
+}
